f648df7b594baca78036d24634ff129234cec8e0
Calvin Hughes <63322016+Ragebo123@users.noreply.github.com>
2023-07-21 13:21:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,16 @@ function play(playerChoice) {
     }
 }
 
+function resetGame() {
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = "";
+
+    // Enable the game buttons again
+    const gameButtons = document.querySelectorAll("#game button");
+    gameButtons.forEach(button => (button.disabled = false));
+}
+
 document.getElementById("rock").addEventListener("click", () => play("rock"));
 document.getElementById("paper").addEventListener("click", () => play("paper"));
 document.getElementById("scissors").addEventListener("click", () => play("scissors"));
+document.getElementById("reset").addEventListener("click", resetGame);
